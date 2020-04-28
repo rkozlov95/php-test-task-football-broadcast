ebc9ad9cff3c1a701d51067e898395a42a0adc97
Roman Kozlov <rkozlov95@mail.ru>
2020-04-29 02:28:36 +0300

    add goal icon in players table

--- src/Service/MatchBuilder.php
+++ src/Service/MatchBuilder.php
@@ -113,8 +113,8 @@ class MatchBuilder
                 case 'goal':
                     $team = $this->getTeamByName($match, $details['team']);
                     $team->addGoal();
+                    $team->getPlayer($details['playerNumber'])->addGoal();
                     break;
-
             }
 
             $match->addMessage(
